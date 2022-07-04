b569915adff2de13301f1024b12d0a5a8cde9e68
Eric <ecas12345@gmail.com>
2022-07-03 23:03:21 -0600

    adding in post method and submit text boxes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AllPostsComponent } from './components/all-posts/all-posts.component';
 import { SearchPostsByUserComponent } from './components/search-posts-by-user/search-posts-by-user.component';
+import { SubmitPostComponent } from './components/submit-post/submit-post.component';
 
 const routes: Routes = [
   { path: 'home', component: AllPostsComponent},
   { path: 'userSearch', component: SearchPostsByUserComponent},
+  { path: 'postMessage', component: SubmitPostComponent},
   { path: '**', component: AllPostsComponent}
 ];
 
---
+++ src/app/components/submit-post/submit-post.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Posts } from 'src/app/interfaces/posts';
+import { PostsService } from 'src/app/services/posts-service.service';
+
+@Component({
+  selector: 'app-submit-post',
+  templateUrl: './submit-post.component.html',
+  styleUrls: ['./submit-post.component.css']
+})
+export class SubmitPostComponent implements OnInit {
+
+  name = new FormControl();
+  title = new FormControl();
+  message = new FormControl();
+  constructor(private postsService: PostsService) { }
+
+  ngOnInit(): void {
+  }
+
+  submitPost = () => {
+    let messageToSubmit = {
+      userid: this.name.value, 
+      title: this.title.value, 
+      message: this.message.value
+    } as Posts;
+    console.log(messageToSubmit);
+    this.postsService.postMessage(messageToSubmit).subscribe(resp => {
+        this.name.reset();
+        this.title.reset();
+        this.message.reset();
+    },
+    error => {
+      alert("failed to post");
+    })
+  }
+}
